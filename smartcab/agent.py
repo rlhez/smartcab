a0585403d6e5dee32547f61583ed9efbc5a1336e
Romain Lhez <romain.lhez@gmail.com>
2016-01-29 16:45:35 +0100

    feedback 1

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -1,4 +1,5 @@
 import random
+import pickle
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
@@ -13,9 +14,11 @@ class LearningAgent(Agent):
         # TODO: Initialize any additional variables here
         self.q = dict()
         self.actions = [None, 'forward', 'left', 'right']
-        self.epsilon=0.1
-        self.alpha = 0.5
-        self.gamma = 0.9
+        self.epsilon=0.5
+        self.alpha = 0.15
+        self.success = 0
+        self.update_nb = 0
+        self.update_list = []
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -42,22 +45,29 @@ class LearningAgent(Agent):
         def getQ(self, state, action):
             return self.q.get((state, action))
 
-        def learnQ(self, state, action, reward, value):
+        def learnQ(self, state, action, reward):
             oldv = self.q.get((state, action), None)
             if oldv is None:
                 self.q[(state, action)] = reward
             else:
-                self.q[(state, action)] = oldv + self.alpha * (value - oldv)
+                self.q[(state, action)] = oldv + self.alpha * (reward - oldv)
+
+            self.update_nb += 1
+            self.update_list.append([{(state, action):(self.q[(state, action)])},self.update_nb])
+            file_object = open('q_values_e_{0}'.format(0.8),'wb')
+            pickle.dump(self.update_list,file_object)
+            file_object.close()
 
         def chooseAction(self, state):
             q = [getQ(self,state, a) for a in self.actions]
             maxQ = max(q)
+            print q
 
             if random.random() < self.epsilon:
                 action = random.choice(self.actions)
-                #minQ = min(q); mag = max(minQ, maxQ)
-                #q = [q[i] + random.random() * mag - .5 * mag for i in range(len(self.actions))] # add random values to all the actions, recalculate maxQ
-                #maxQ = max(q)
+                self.epsilon = 0.99 * self.epsilon # epsilon decays over time
+                print "epsilon"
+                print self.epsilon
 
             else:
               count = q.count(maxQ)
@@ -68,23 +78,22 @@ class LearningAgent(Agent):
                   i = q.index(maxQ)
 
               action = self.actions[i]
+            print 'Exploration of state space is {0:.2f} %'.format((float(len(self.q.keys())) / 512) * 100)
 
             return action
-
-
-
         
         if agent_policy == 'basic':
-          print 'rd'
           action = rd_action
         else:
           action = chooseAction(self,self.state)
         # Execute action and get reward
         reward = self.env.act(self, action)
         if agent_policy != 'basic':
-          learnQ(self,self.state,action,reward,reward)
+          learnQ(self,self.state,action,reward)
 
-
+        if reward > 10:
+            self.success += 1
+            print "Destination reached {0} times".format(self.success)
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
@@ -98,7 +107,7 @@ def run():
     e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
+    sim = Simulator(e, update_delay=0.01)  # reduce update_delay to speed up simulation
     sim.run(n_trials=100)  # press Esc or close pygame window to quit
 
 
